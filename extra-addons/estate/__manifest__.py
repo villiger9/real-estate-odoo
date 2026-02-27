5e4aa04015c4018c1e40bee1503dc9017b9eb15e
villiger9 <ali.shaker93@gmail.com>
2026-02-27 19:44:59 +0300

    added action and basic views and security

--- extra-addons/estate/__manifest__.py
+++ extra-addons/estate/__manifest__.py
@@ -1,8 +1,12 @@
 {
     'name': "estate",
+    'version': '1.0',
     'depends': ['base'],
     'description': """Description text""",
     'application': True,
-
-
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
 }
